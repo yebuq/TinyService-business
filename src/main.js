ea3eb7bd27a0d2242fccc1d7160e183af7375532
yebuq <zhangjian782@jd.com>
2020-10-29 18:21:46 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -3,13 +3,28 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {setPublicPath} from 'systemjs-webpack-interop'
+import singleSpaVue from 'single-spa-vue'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      h(App)
+    },
+    router
+  }
 })
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
